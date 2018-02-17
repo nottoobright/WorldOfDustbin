a7ba4b753d825edce296e61edf9316e5bd458c0b
Jatin Parab <sulphurgfx@gmail.com>
2018-02-18 05:24:38 +0530

    Fbsend

--- testing/fbsend.py
+++ testing/fbsend.py
@@ -5,9 +5,11 @@ firebase = firebase.FirebaseApplication('https://dj-hack.firebaseio.com', None)
 result = firebase.get('/dustbins/', None)
 #print(result)
 data = {'cash': 50, 'currentUser': '', 'parent': '', 'percentFull': 22, 'queryText': 'Daddy 2 wants to buy a dustbin from Daddy 1', 'queryType': 0, 'streak': 0}
-data2 = {'cash': 50, 'currentUser': '', 'parent': '', 'percentFull': 82, 'queryText': 'Daddy 1 agrees to sell a dustbin to Daddy 2', 'queryType': 1, 'streak': 0}
+data2 = {'cash': 50, 'currentUser': '', 'parent': '', 'percentFull': 82, 'queryText': 'Daddy 1 agrees to sell a dustbin to Daddy 2', 'queryType': 1, 'streak': 50}
+data3 = {'cash': 300, 'currentUser': '', 'parent': '', 'percentFull': 0, 'queryText': 'data3', 'queryType': -1, 'streak': 0}
+data4 = {'cash': 300, 'currentUser': '', 'parent': '', 'percentFull': 0, 'queryText': 'data4', 'queryType': -1, 'streak': 50}
 #data = json.dumps(data)
-firebase.put('/dustbins', "1518880087182", data)
+firebase.put('/dustbins', "1518880087182", data3)
 
-firebase.put('/dustbins',"1518880134851",data2)
+firebase.put('/dustbins',"1518880134851",data4)
 
