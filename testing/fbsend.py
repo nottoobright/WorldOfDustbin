793953a5fd5aec2fddd2f64e4bdcbde4330cfa58
Jatin Parab <sulphurgfx@gmail.com>
2018-02-17 20:57:52 +0530

    Images

--- testing/fbsend.py
+++ testing/fbsend.py
@@ -1,6 +1,9 @@
 from firebase import firebase
+import json
+
 firebase = firebase.FirebaseApplication('https://dj-hack.firebaseio.com', None)
 result = firebase.get('/dustbins/1518880087182', None)
-
-print(result)
+data = {'cash': 70, 'currentUser': '', 'parent': '', 'percentFull': 100, 'queryText': 'Daddy 2 wants to buy a dustbin from Daddy 1', 'queryType': 0, 'streak': 0}
+data = json.dumps(data)
+firebase.post('/dustbins/1518880087182', data )
 
