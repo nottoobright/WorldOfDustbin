7d4243fdf42adc364febfd01f2aa1755b08090ac
Jatin Parab <sulphurgfx@gmail.com>
2018-02-17 20:52:47 +0530

    adkk

--- testing/2send.py
+++ testing/2send.py
@@ -1,6 +1,5 @@
 from firebase import firebase
 firebase = firebase.FirebaseApplication('https://dj-hack.firebaseio.com', None)
-result = firebase.get('/dustbins/1518880087182', None)
-
+result = firebase.get('/dustbins', None)
 print(result)
 
